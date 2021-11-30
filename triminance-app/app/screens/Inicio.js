10a4a5b7b0b2cd58bc4ea5452db112c08bad5b9f
johan2612 <johanenrriquer@gmail.com>
2021-11-30 17:49:30 -0500

    s

--- triminance-app/app/screens/Inicio.js
+++ triminance-app/app/screens/Inicio.js
@@ -2,15 +2,26 @@ import React from "react";
 import { View, Text } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import Logo from '../../assets/img/LOGO2.png';
+import Mano from '../../assets/img/ref-icon2.png'
 
 const Inicio = () => {
     return (
         <SafeAreaView>
-
+        
         <LinearGradient colors={["#EA0451", "#FF682F"]}>
-        <Logo/>    
-        </LinearGradient>
+        
+            <View>
+                <Logo/>
+                <Text>Hola,Usuario</Text>
+            </View>    
+            <hr/>
 
+            <View>
+                <Mano/>
+                <Text>CUENTO CON</Text>
+            </View>
+        
+        </LinearGradient>
 
         </SafeAreaView>
     );
