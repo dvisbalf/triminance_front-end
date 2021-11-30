da6823dbac4803816fdc33483245cec531896ed7
johan2612 <johanenrriquer@gmail.com>
2021-11-30 15:42:09 -0500

    s

---
+++ triminance-app/App.js
@@ -0,0 +1,50 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { enableScreens } from "react-native-screens";
+import { useFonts } from 'expo-font';
+
+enableScreens();
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./app/screens/Login";
+import Registro from "./app/screens/Registro";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [loaded] = useFonts({
+    GraviolaSoftMedium: require('./assets/fonts/GraviolaSoft-Medium.otf'),
+    RubikRegular: require('./assets/fonts/Rubik-Regular.ttf'),
+    RubikBold: require('./assets/fonts/Rubik-Bold.ttf'),
+    GraviolaRegular: require('./assets/fonts/Graviola-Regular.otf')
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registro" component={Registro} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ triminance-app/app/screens/Registro.js
@@ -0,0 +1,220 @@
+import React, { useState } from "react";
+import {
+  TouchableHighlight,
+  TextInput,
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+} from "react-native";
+import LOGO from "../../assets/img/LOGO.svg";
+import { LinearGradient } from "expo-linear-gradient";
+import { Picker } from "@react-native-picker/picker";
+import { CheckBox } from "react-native-elements";
+
+const Registro = () => {
+  const [selectedValue, setSelectedValue] = useState("pais");
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={stylesR.container}>
+        <LinearGradient
+          colors={["#EA0451", "#FF682F"]}
+          style={stylesR.gradient}
+        >
+          <LOGO style={stylesR.logo} />
+          <Text style={stylesR.text2}>El dinero de tus sueños,</Text>
+          <Text style={stylesR.text3}> al precio justo.</Text>
+        </LinearGradient>
+
+        <Text style={stylesR.text}>Registro de Usuario</Text>
+
+        <View>
+          <TextInput
+            style={stylesR.border}
+            name="email"
+            type="email"
+            placeholder="Email"
+            required
+          />
+
+          <TextInput
+            style={stylesR.border}
+            name="Cemail"
+            type="email"
+            placeholder="Confirmar email"
+            required
+          />
+
+          <TextInput
+            style={stylesR.border}
+            name="telefono"
+            type="number"
+            placeholder="Telefono movil"
+            required
+          />
+
+          <TextInput
+            style={stylesR.border}
+            name="user"
+            type="text"
+            placeholder="Usuario"
+            required
+          />
+
+          <View style={stylesR.comboborde}>
+            <Picker
+              selectedValue={selectedValue}
+              style={stylesR.pais}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+              required
+            >
+              <Picker.Item label="Pais" value="" />
+              <Picker.Item label="java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+
+          <View style={stylesR.comboborde}>
+            <Picker
+              style={stylesR.pais}
+              selectedValue={selectedValue}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+              required
+            >
+              <Picker.Item label="Estado" value="" />
+              <Picker.Item label="java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+
+          <View style={stylesR.comboborde}>
+            <Picker
+              selectedValue={selectedValue}
+              style={stylesR.pais}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+              required
+            >
+              <Picker.Item label="Ciudad" value="" />
+              <Picker.Item label="java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+
+          <CheckBox
+          center 
+          title="Certifico que estoy de acuerdo con los"
+          
+          />
+
+          <Text>términos y condiciones</Text>
+
+          <TouchableHighlight>
+            <Text>Registrarse</Text>
+          </TouchableHighlight>
+        </View>
+
+        <TouchableHighlight>
+          <Text>Volver</Text>
+        </TouchableHighlight>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Registro;
+
+const stylesR = StyleSheet.create({
+  header: {
+    paddingTop: 35,
+    opacity: 1,
+    color: "#fff",
+    fontSize: 15,
+    paddingBottom: "10%",
+    textAlign: "center",
+    borderBottomRightRadius: 117,
+    borderBottomLeftRadius: 117,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+
+  gradient: {
+    width: "100%",
+    height: 226,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+  },
+
+  text: {
+    color: "#002D75",
+    fontSize: 30,
+    textAlign: "center",
+    marginTop: "5%",
+  },
+
+  text2: {
+    color: "white",
+    paddingLeft: "20%",
+    fontSize: 30,
+  },
+
+  text3: {
+    color: "white",
+    paddingLeft: "28%",
+    fontSize: 30,
+  },
+
+  logo: {
+    height: 70,
+    width: 70,
+    marginLeft: "40%",
+  },
+
+  centrado: {
+    alignItems: "center",
+    textAlign: "center",
+  },
+
+  border: {
+    borderWidth: 1,
+    borderRadius: 60,
+    paddingLeft: 10,
+    marginLeft: "7%",
+    marginRight: "7%",
+    marginTop: "6%",
+    padding: 0,
+  },
+
+  terminos: {
+    flexDirection: "row",
+    marginLeft: "13%",
+  },
+
+  alinear: {
+    marginTop: "4%",
+    fontSize: 17,
+  },
+
+  pais: {
+    height: 28,
+    color: "black",
+  },
+  comboborde: {
+    borderRadius: 60,
+    paddingLeft: 10,
+    marginLeft: "7%",
+    marginRight: "7%",
+    marginTop: "6%",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
