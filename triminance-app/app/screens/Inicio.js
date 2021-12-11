5e7d1c065f84fc4e73d624c26a9cd54f48151d7c
johan2612 <johanenrriquer@gmail.com>
2021-12-11 11:25:51 -0500

    aaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaa

---
+++ triminance-app/app/screens/Inicio.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const Inicio = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+        <LinearGradient colors={["#EA0451", "#FF682F"]} style={style.gradient}>
+          <View style={style.flex}>
+            <Logo style={style.logo} />
+            <Text>Hola,usuario</Text>
+          </View>
+        </LinearGradient>
+    </SafeAreaView>
+  );
+};
+
+export default Inicio;
+
+const style = StyleSheet.create({
+  gradient: {
+    width: "100%",
+    height: 226,
+    borderBottomLeftRadius: 100,
+    borderBottomRightRadius: 100,
+  },
+  logo: {
+    marginTop: 42,
+    marginLeft: "45%",
+    marginBottom: 17,
+  },
+  flex: {
+    flexDirection: "row",
+  },
+});
